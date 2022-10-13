55f22604870a6de6eb274ea4b2bfb6027ba5b9b8
Ye-Thiha-scm <scm.yethiha@gmail.com>
2022-10-13 17:05:45 +0630

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,82 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { BycategoryComponent } from './bycategory/bycategory.component';
+import { DetailsComponent } from './details/details.component';
+import { CategoryComponent } from './category/category.component';
+import { CategoryDetailsComponent } from './category/category-details/category-details.component';
+import { CategoryAddComponent } from './category/category-add/category-add.component';
+import { CategoryEditComponent } from './category/category-edit/category-edit.component';
+import { PostComponent } from './post/post.component';
+import { PostDetailsComponent } from './post/post-details/post-details.component';
+import { PostAddComponent } from './post/post-add/post-add.component';
+import { PostEditComponent } from './post/post-edit/post-edit.component';
+import {  HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { TokenInterceptor } from './interceptors/token.interceptor';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    AdminComponent,
+    BycategoryComponent,
+    DetailsComponent,
+    CategoryComponent,
+    CategoryDetailsComponent,
+    CategoryAddComponent,
+    CategoryEditComponent,
+    PostComponent,
+    PostDetailsComponent,
+    PostAddComponent,
+    PostEditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatSortModule,
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatMenuModule,
+    MatToolbarModule,
+
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
